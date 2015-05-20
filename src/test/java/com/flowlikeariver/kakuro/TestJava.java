1394570f7dad5fc2ccbbfd8340e7e730474f7f70
James Petry <gavilancomun@gmail.com>
2015-05-20 16:28:01 +0100

    refactor

--- src/test/java/com/flowlikeariver/kakuro/TestJava.java
+++ src/test/java/com/flowlikeariver/kakuro/TestJava.java
@@ -1,20 +1,26 @@
 package com.flowlikeariver.kakuro;
 
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Set;
 import static org.junit.Assert.assertEquals;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestJava {
 
+@Ignore
 @Test
 public void testSet() {
   Set<Integer> t = new HashSet<>(Arrays.asList(1, 2, 3, 1, 2, 3));
   assertEquals(3, t.size());
 }
 
+@Ignore
 @Test
 public void testLinked() {
   LinkedList<Integer> list = new LinkedList<>();
@@ -25,4 +31,28 @@ public void testLinked() {
   assertEquals(2, (int) list.get(1));
   assertEquals(3, (int) list.get(0));
 }
+
+@Ignore
+@Test
+public void testFonts() {
+  Arrays.stream(java.awt.GraphicsEnvironment
+          .getLocalGraphicsEnvironment()
+          .getAvailableFontFamilyNames())
+          .forEach(System.out::println);
+  System.out.println("---");
+  System.out.println(RenderingHints.KEY_TEXT_ANTIALIASING);
+  System.out.println(RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+  System.out.println(RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+  System.out.println(RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT);
+  System.out.println(RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+  System.out.println("---");
+  Toolkit tk = Toolkit.getDefaultToolkit();
+  Map desktopHints = (Map) tk.getDesktopProperty("awt.font.desktophints");
+  if (null != desktopHints) {
+    desktopHints.keySet().forEach(k -> {
+      System.out.println(k + " = " + desktopHints.get(k));
+    });
+  }
+}
+
 }
