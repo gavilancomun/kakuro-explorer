e572a274a95c4b35039000807cd2f2f021253135
James Petry <gavilancomun@gmail.com>
2015-05-21 16:06:10 +0100

    refactor

--- src/main/java/com/flowlikeariver/kakuro/Sum.java
+++ src/main/java/com/flowlikeariver/kakuro/Sum.java
@@ -19,22 +19,22 @@ public Sum(int total, List<ValueCell> valueCells) {
 }
 
 // All different is part of the definition of a kakuro puzzle
-private boolean areAllDifferent(List<Integer> candidates) {
-  return (new HashSet<>(candidates).size() == candidates.size());
+private static boolean areAllDifferent(List<Integer> candidates) {
+  return new HashSet<>(candidates).size() == candidates.size();
 }
 
-private List<Integer> copyAdd(List<Integer> vs, int v) {
+private static List<Integer> copyAdd(List<Integer> vs, int v) {
   return Stream.concat(vs.stream(), Stream.of(v)).collect(toList());
 }
 
-private Stream<List<Integer>> permute(int pos, int target, List<Integer> soFar) {
+private static Stream<List<Integer>> permute(List<ValueCell> cells, int pos, int target, List<Integer> soFar) {
   if (target >= 1) {
     if (pos == (cells.size() - 1)) {
       return Stream.of(copyAdd(soFar, target));
     }
     else {
       return cells.get(pos).getValues().stream()
-              .flatMap(v -> permute(pos + 1, target - v, copyAdd(soFar, v)));
+              .flatMap(v -> permute(cells, pos + 1, target - v, copyAdd(soFar, v)));
     }
   }
   else {
@@ -43,21 +43,25 @@ private Stream<List<Integer>> permute(int pos, int target, List<Integer> soFar) 
 }
 
 // Exhaustive search for possible solutions
-private Stream<List<Integer>> permuteAll() {
-  return permute(0, total, Collections.EMPTY_LIST);
+private static Stream<List<Integer>> permuteAll(List<ValueCell> cells, int target) {
+  return permute(cells, 0, target, Collections.EMPTY_LIST);
 }
 
-public int solveStep() {
+public static int solveStep(List<ValueCell> cells, int target) {
   List<Possible> possibles = cells.stream().map(Possible::new).collect(toList());
   int last = cells.size() - 1;
-  permuteAll()
+  permuteAll(cells, target)
           .filter(perm -> cells.get(last).isPossible(perm.get(last)))
-          .filter(this::areAllDifferent)
+          .filter(Sum::areAllDifferent)
           .forEach(perm -> IntStream.rangeClosed(0, last)
                   .forEach(i -> possibles.get(i).add(perm.get(i))));
   return possibles.stream()
           .mapToInt(Possible::update)
           .sum();
+}
+
+public int solveStep() {
+  return solveStep(cells, total);
 }
 
 }
